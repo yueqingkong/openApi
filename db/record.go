b4a914aa8078036b0b105358e54bf48e89a15146
yueqingkong <762396990@qq.com>
2022-01-10 19:25:54 +0800

    添加 止损价

--- db/record.go
+++ db/record.go
@@ -9,22 +9,24 @@ import (
 
 // MarginRecord 交易记录
 type Record struct {
-	Id         int64
-	Plat       string    `xorm:"varchar(255) plat index(plat-symbol-period)"`   // 平台名称
-	Symbol     string    `xorm:"varchar(255) symbol index(plat-symbol-period)"` // Token
-	Period     string    `xorm:"varchar(255) period index(plat-symbol-period)"` // 周期 spot|week|quarter
-	Operation  int32     `xorm:"int"`                                           // 1: 开多 2: 开空 3: 平仓
-	Position   int32     `xorm:"int"`                                           // 加仓层数
-	Price      float32   `xorm:"float"`                                         // 当前价格
-	AvgPrice   float32   `xorm:"float"`                                         // 均价
-	Used       float32   `xorm:"float"`                                         // 已开仓Token
-	Size       float32   `xorm:"float"`                                         // 开仓张数
-	Total      float32   `xorm:"float"`                                         // 当前账户总值
-	Detail     string    `xorm:"detail text"`                                   // 描述 usd->token | ust<-token
-	Profit     float32   `xorm:"float"`                                         // 收益
-	ProfitRate float32   `xorm:"float"`                                         // 收益率
-	TotalRate  float32   `xorm:"float"`                                         // 总收益率
-	CreateTime time.Time `json:"create_time" xorm:"create_time DateTime index"` // 时间
+	Id            int64
+	Plat          string    `xorm:"varchar(255) plat index(plat-symbol-period)"`   // 平台名称
+	Symbol        string    `xorm:"varchar(255) symbol index(plat-symbol-period)"` // Token
+	Period        string    `xorm:"varchar(255) period index(plat-symbol-period)"` // 周期 spot|week|quarter
+	Operation     int32     `xorm:"int"`                                           // 1: 开多 2: 开空 3: 平仓
+	Position      int32     `xorm:"int"`                                           // 加仓层数
+	Price         float32   `xorm:"float"`                                         // 当前价格
+	AvgPrice      float32   `xorm:"float"`                                         // 均价
+	Used          float32   `xorm:"float"`                                         // 已开仓Token
+	Size          float32   `xorm:"float"`                                         // 开仓张数
+	Total         float32   `xorm:"float"`                                         // 当前账户总值
+	LossPrice     float32   `xorm:"float"`                                         // 止损价
+	EstimatedLoss float32   `xorm:"float"`                                         // 预计亏损
+	Detail        string    `xorm:"detail text"`                                   // 描述 usd->token | ust<-token
+	Profit        float32   `xorm:"float"`                                         // 收益
+	ProfitRate    float32   `xorm:"float"`                                         // 收益率
+	TotalRate     float32   `xorm:"float"`                                         // 总收益率
+	CreateTime    time.Time `json:"create_time" xorm:"create_time DateTime index"` // 时间
 }
 
 func (self *Record) Insert(pt conset.PLAT, symbol conset.SYMBOL, period conset.PERIOD) error {
