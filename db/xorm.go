fa9179338b24b22b1944d4a53f1c47736eb911d1
yueqingkong <762396990@qq.com>
2022-01-11 12:01:07 +0800

    余额记录

--- db/xorm.go
+++ db/xorm.go
@@ -23,7 +23,7 @@ func ConnectSQL(name,user,host,port,password string) {
 	}
 
 	engine.ShowSQL(true)
-	err = engine.Sync2(new(Coin), new(Account), new(Record))
+	err = engine.Sync2(new(Coin), new(Account), new(Record),new(AccountDaily))
 	if err != nil {
 		log.Fatal("[MySql] 同步表失败", err)
 	}
