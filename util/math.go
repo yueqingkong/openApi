f9ff3212be06d998a7eb40666c81691c0587d0d5
yueqingkong <762396990@qq.com>
2022-01-13 15:21:27 +0800

    开N次方

--- util/math.go
+++ util/math.go
@@ -49,3 +49,20 @@ func Min(values ...float32) float32 {
 func Floor(value float32) float32 {
 	return float32(math.Floor(float64(value)))
 }
+
+func SqrtN(m float32, n float32) float32 {
+	m64 := float64(m)
+	n64 := float64(n)
+	return float32(SqrtN64(m64, n64))
+}
+
+// 开N次方
+func SqrtN64(m float64, n float64) float64 {
+	z := float64(1)
+	tmp := float64(0)
+	for math.Abs(tmp-z) > 0.0000000001 {
+		tmp = z
+		z = z - (math.Pow(z, n)-m)/(n*math.Pow(z, n-1))
+	}
+	return z
+}
